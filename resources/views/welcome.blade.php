17bb566c2571e1d5fe031cd1d4c318c9a89ac71d
Xuying <wuxvying@gmail.com>
2024-02-23 10:27:44 -0500

    version pour webdev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
     <div class="col-lg-6 px-0">
         <h1 class="display-4">Maisoneuve College</h1>
         <p class="lead my-3"></p>
-        <p class="lead mb-0"><a href="{{ route('edutiant.index') }}" class="text-body-emphasis fw-bold">Liste d'Edutiants</a></p>
+        <p class="lead mb-0">
+            <a href="{{ route('edutiant.index') }}" class="text-body-emphasis fw-bold">Liste d'Edutiants</a>
+        </p>
     </div>
 </div>
 
